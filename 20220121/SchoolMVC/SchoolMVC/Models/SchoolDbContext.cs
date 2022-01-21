0cca0f6aca4926eba7fbc526aed55ba035548f82
Burak Akyüz <burak.akyuz@bilgeadamboost.com>
2022-01-21 13:06:16 +0300

    21.01.2022 Mvc Giris

---
+++ 20220121/SchoolMVC/SchoolMVC/Models/SchoolDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SchoolMVC.Models
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) :
+            base(options) //Ayarları taşır.(Hangi Db, Hangi sunucu,)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                new Student() {Id = 1, Name = "Bilge", Surname = "Lorem"},
+                new Student() {Id = 2, Name = "Ezgi", Surname= "Ipsum"},
+                new Student() {Id = 3, Name = "Koray", Surname = "Amet"},
+                new Student() {Id = 4, Name = "Burak", Surname = "Elit"});
+
+        }
+    }
+}
---
+++ 20220121/SchoolMVC/SchoolMVC/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolMVC.Models
+{
+    public class Student
+    {
+        public int Id  { get; set; }
+        [Required,MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required,MaxLength(50)]
+        public string Surname { get; set; }
+    }
+}
