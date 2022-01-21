19fa9e4f6e629a2f7b4d050853a73472bbcb80d7
BilgeAdam <user.user@baogrenci.com>
2022-01-21 17:47:39 +0300

    KanGrubu

---
+++ 20220121/KanGrubuMvc/KanGrubuMvc/BaseClass/KanGrubu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanGrubuMvc.BaseClass
+{
+    public class KanGrubu
+    {
+        public string Grup { get; set; }
+        public char Rh { get; set; }
+
+    }
+}
---
+++ 20220121/KanGrubuMvc/KanGrubuMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KanGrubuMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
