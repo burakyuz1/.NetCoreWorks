c496f2fd3d5b931339db92504703a48d4779f2c5
burakyuz1 <burakyuzz@gmail.com>
2022-02-08 10:23:50 +0300

    8.2.2022 » Area

---
+++ 20220208/BizimMarket/BizimMarket/Models/Product.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BizimMarket.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required,MaxLength(100)]
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductImage { get; set; }
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
